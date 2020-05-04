e10c57665fff52c93aaa2db80542972a37ccd2ce
KarbordComputerApp <62652601+KarbordComputerApp@users.noreply.github.com>
2020-05-04 19:43:27 +0330

    افزودن گزارش تراز دفاتر

--- WebKarbord/Scripts/R_AFI_TrzAcc.js
+++ WebKarbord/Scripts/R_AFI_TrzAcc.js
@@ -1,1 +1,310 @@
-﻿
+﻿var ViewModel = function () {
+    var self = this;
+    var ace = sessionStorage.ace;
+    var sal = sessionStorage.sal;
+    var group = sessionStorage.group;
+    var flagupdateHeader = 0;
+    var server = localStorage.getItem("ApiAddress");
+
+    self.AccList = ko.observableArray([]); // ليست حساب ها
+    self.MkzList = ko.observableArray([]); // ليست مرکز هزینه
+    self.OprList = ko.observableArray([]); // ليست پروژه ها
+
+    self.TrzAccList = ko.observableArray([]); // لیست گزارش 
+
+    var AccUri = server + '/api/Web_Data/Acc/'; // آدرس حساب ها
+    var MkzUri = server + '/api/Web_Data/Mkz/'; // آدرس مرکز هزینه
+    var OprUri = server + '/api/Web_Data/Opr/'; // آدرس پروژه 
+
+    var TrzAccUri = server + '/api/ReportAcc/TrzAcc/'; // آدرس گزارش 
+
+    self.AzDate = ko.observable(sessionStorage.BeginDate);
+    self.TaDate = ko.observable(sessionStorage.EndDate);
+
+
+    var AccCode = '';
+    var counterAcc = 0;
+    var list_AccSelect = new Array();
+
+    var MkzCode = '';
+    var counterMkz = 0;
+    var list_MkzSelect = new Array();
+
+    var OprCode = '';
+    var counterOpr = 0;
+    var list_OprSelect = new Array();
+
+
+    //Get Acc List
+    function getAccList() {
+        ajaxFunction(AccUri + ace + '/' + sal + '/' + group, 'GET').done(function (data) {
+            self.AccList(data);
+        });
+    }
+
+    //Get Opr List
+    function getOprList() {
+        ajaxFunction(OprUri + ace + '/' + sal + '/' + group, 'GET').done(function (data) {
+            self.OprList(data);
+        });
+    }
+
+    //Get  Mkz List
+    function getMkzList() {
+        ajaxFunction(MkzUri + ace + '/' + sal + '/' + group, 'GET').done(function (data) {
+            self.MkzList(data);
+        });
+    }
+
+
+    getAccList();
+    getOprList();
+    getMkzList();
+
+
+    $('#nameAcc').val('همه موارد');
+    $('#nameOpr').val('همه موارد');
+    $('#nameMkz').val('همه موارد');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    self.sortType = "ascending";
+    self.currentColumn = ko.observable("");
+
+    self.iconTypeCode = ko.observable("");
+    self.iconTypeName = ko.observable("");
+    self.iconTypeSpec = ko.observable("");
+
+
+
+    self.currentPageAcc = ko.observable();
+    self.pageSizeAcc = ko.observable(10);
+    self.currentPageIndexAcc = ko.observable(0);
+
+    self.filterAcc0 = ko.observable("");
+    self.filterAcc1 = ko.observable("");
+    self.filterAcc2 = ko.observable("");
+
+    self.filterAccList = ko.computed(function () {
+
+        self.currentPageIndexAcc(0);
+        var filter0 = self.filterAcc0().toUpperCase();
+        var filter1 = self.filterAcc1();
+        var filter2 = self.filterAcc2();
+
+        if (!filter0 && !filter1 && !filter2) {
+            return self.AccList();
+        } else {
+            tempData = ko.utils.arrayFilter(self.AccList(), function (item) {
+                result =
+                    ko.utils.stringStartsWith(item.Code.toString().toLowerCase(), filter0) &&
+                    (item.Name == null ? '' : item.Name.toString().search(filter1) >= 0) &&
+                    (item.Spec == null ? '' : item.Spec.toString().search(filter2) >= 0)
+                return result;
+            })
+            return tempData;
+        }
+    });
+
+
+    self.currentPageAcc = ko.computed(function () {
+        var pageSizeAcc = parseInt(self.pageSizeAcc(), 10),
+            startIndex = pageSizeAcc * self.currentPageIndexAcc(),
+            endIndex = startIndex + pageSizeAcc;
+        return self.filterAccList().slice(startIndex, endIndex);
+    });
+
+    self.nextPageAcc = function () {
+        if (((self.currentPageIndexAcc() + 1) * self.pageSizeAcc()) < self.filterAccList().length) {
+            self.currentPageIndexAcc(self.currentPageIndexAcc() + 1);
+        }
+    };
+
+    self.previousPageAcc = function () {
+        if (self.currentPageIndexAcc() > 0) {
+            self.currentPageIndexAcc(self.currentPageIndexAcc() - 1);
+        }
+    };
+
+    self.firstPageAcc = function () {
+        self.currentPageIndexAcc(0);
+    };
+
+    self.lastPageAcc = function () {
+        countAcc = parseInt(self.filterAccList().length / self.pageSizeAcc(), 10);
+        if ((self.filterAccList().length % self.pageSizeAcc()) == 0)
+            self.currentPageIndexAcc(countAcc - 1);
+        else
+            self.currentPageIndexAcc(countAcc);
+    };
+
+    self.sortTableAcc = function (viewModel, e) {
+        var orderProp = $(e.target).attr("data-column")
+        self.currentColumn(orderProp);
+        self.AccList.sort(function (left, right) {
+            leftVal = left[orderProp];
+            rightVal = right[orderProp];
+            if (self.sortType == "ascending") {
+                return leftVal < rightVal ? 1 : -1;
+            }
+            else {
+                return leftVal > rightVal ? 1 : -1;
+            }
+        });
+        self.sortType = (self.sortType == "ascending") ? "descending" : "ascending";
+
+        self.iconTypeCode('');
+        self.iconTypeName('');
+        self.iconTypeSpec('');
+
+
+        if (orderProp == 'Code') self.iconTypeCode((self.sortType == "ascending") ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down");
+        if (orderProp == 'Name') self.iconTypeName((self.sortType == "ascending") ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down");
+        if (orderProp == 'Spec') self.iconTypeSpec((self.sortType == "ascending") ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down");
+    };
+
+    self.PageCountView = function () {
+        sessionStorage.invSelect = $('#invSelect').val();
+        invSelect = $('#invSelect').val() == '' ? 0 : $('#invSelect').val();
+        select = $('#pageCountSelector').val();
+        getIDocH(select, invSelect);
+    }
+
+
+
+    $('#refreshAcc').click(function () {
+        Swal.fire({
+            title: 'تایید به روز رسانی ؟',
+            text: "لیست تحویل دهنده / گیرنده به روز رسانی شود ؟",
+            type: 'info',
+            showCancelButton: true,
+            cancelButtonColor: '#3085d6',
+            cancelButtonText: 'خیر',
+            allowOutsideClick: false,
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'بله'
+        }).then((result) => {
+            if (result.value) {
+                $("div.loadingZone").show();
+                getAccList();
+                $("div.loadingZone").hide();
+            }
+        })
+    })
+
+
+    self.AddAcc = function (item) {
+
+        AccCode = item.Code;
+        find = false;
+        list_AccSelect.forEach(function (item, key) {
+            if (item == AccCode) {
+                find = true;
+            }
+        });
+
+        if (find == false) {
+            $('#TableBodyListAcc').append(
+                '<tr data-bind="">'
+                + ' <td data-bind="text: Code">' + item.Code + '</td > '
+                + ' <td data-bind="text: Name">' + item.Name + '</td > '
+                + ' <td data-bind="text: Spec">' + item.Spec + '</td > '
+                + '</tr>'
+            );
+            list_AccSelect[counterAcc] = item.Code;
+            counterAcc = counterAcc + 1;
+        }
+    };
+
+
+    self.AddAllAcc = function () {
+        list_AccSelect = new Array();
+        list = self.AccList();
+        $("#TableBodyListAcc").empty();
+        for (var i = 0; i < list.length; i++) {
+            $('#TableBodyListAcc').append(
+                '  <tr data-bind="">'
+                + ' <td data-bind="text: Code">' + list[i].Code + '</td > '
+                + ' <td data-bind="text: Name">' + list[i].Name + '</td > '
+                + ' <td data-bind="text: Spec">' + list[i].Spec + '</td > '
+                + '</tr>'
+            );
+            list_AccSelect[i] = list[i].Code;
+            counterAcc = i + 1;
+        }
+    };
+
+
+    self.DelAllAcc = function () {
+        list_AccSelect = new Array();
+        counterAcc = 0;
+        $("#TableBodyListAcc").empty();
+    };
+
+
+    $('#modal-Acc').on('hide.bs.modal', function () {
+        if (counterAcc > 0)
+            $('#nameAcc').val(counterAcc + ' مورد انتخاب شده ')
+        else
+            $('#nameAcc').val('همه موارد');
+    });
+
+    $('#modal-Acc').on('shown.bs.modal', function () {
+        $('.fix').attr('class', 'form-line focused fix');
+    });
+
+
+
+
+
+
+    $('.fix').attr('class', 'form-line date focused fix');
+
+};
+
+ko.applyBindings(new ViewModel());
