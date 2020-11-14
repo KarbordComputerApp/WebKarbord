04369f05948d928b98cbb74f83999171ad000b16
parto <parto@DESKTOP-9B5K4FE>
2020-11-14 17:41:31 +0330

    تغیر نسخه

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -14,11 +14,11 @@ namespace WebKarbord.Controllers.Unit
 {
     public class UnitPublic
     {
-        public static string titleVer = "ورژن تست : ";
-        public static string titleVerNumber = "270";
+       // public static string titleVer = "ورژن تست : ";
+        //public static string titleVerNumber = "270";
 
-        //public static string titleVer = "ورژن : ";
-        //public static string titleVerNumber = "1007";
+        public static string titleVer = "ورژن : ";
+       public static string titleVerNumber = "1008";
 
 
         //Server.MapPath("ini/SqlServerConfig.Ini");
