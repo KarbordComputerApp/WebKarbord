3920c3af005108a9181213292dddbfab113e0131
parto <parto@DESKTOP-9B5K4FE>
2020-12-19 17:24:35 +0330

    رفع اشکال

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -14,11 +14,11 @@ namespace WebKarbord.Controllers.Unit
 {
     public class UnitPublic
     {
-       //public static string titleVer = "ورژن تست : ";
-       //public static string titleVerNumber = "299";
+       public static string titleVer = "ورژن تست : ";
+       public static string titleVerNumber = "300";
 
-       public static string titleVer = "ورژن : ";
-       public static string titleVerNumber = "1010";
+       //public static string titleVer = "ورژن : ";
+       //public static string titleVerNumber = "1010";
 
 
         //Server.MapPath("ini/SqlServerConfig.Ini");
