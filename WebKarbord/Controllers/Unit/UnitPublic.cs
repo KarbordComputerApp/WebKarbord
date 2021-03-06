1f1e9f25767b990bb39b56369f2597bb8fe43f34
parto <parto@DESKTOP-9B5K4FE>
2021-03-06 17:35:27 +0330

    تغییر ورژن

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,7 +16,7 @@ namespace WebKarbord.Controllers.Unit
     public class UnitPublic
     {
         public static string titleVer = "ورژن تست : ";
-        public static string titleVerNumber = "332";
+        public static string titleVerNumber = "333";
 
         // public static string titleVer = "ورژن : ";
         //  public static string titleVerNumber = "1012";
