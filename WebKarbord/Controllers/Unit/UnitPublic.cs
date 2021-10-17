23e68e0dcac84b953ff010391c3e20bf86c73d11
parto <parto@DESKTOP-9B5K4FE>
2021-10-17 15:17:36 +0330

    رفع اشکال نمایش حساب ها در موبایل

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,7 +16,7 @@ namespace WebKarbord.Controllers.Unit
     public class UnitPublic
     {
         public static string titleVer = "ورژن تست : ";
-        public static string titleVerNumber = "602"; 
+        public static string titleVerNumber = "603"; 
 
         //public static string titleVer = "ورژن : ";
         //public static string titleVerNumber = "1020";
