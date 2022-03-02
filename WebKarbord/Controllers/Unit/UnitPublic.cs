8384bbca6b25f7558085667da8916df1eea1201f
KarbordComputerApp <62652601+KarbordComputerApp@users.noreply.github.com>
2022-03-02 11:44:44 +0330

    رفع اشکال

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,10 +16,10 @@ namespace WebKarbord.Controllers.Unit
     public class UnitPublic
     {
        public static string titleVer = "ورژن تست";
-        public static string titleVerNumber = "683"; 
+       public static string titleVerNumber = "684"; 
 
-       // public static string titleVer = "ورژن";
-      //  public static string titleVerNumber = "1026";
+        //public static string titleVer = "ورژن";
+        //public static string titleVerNumber = "1026";
 
         //public static string MAC;
         //public static string IP4;
