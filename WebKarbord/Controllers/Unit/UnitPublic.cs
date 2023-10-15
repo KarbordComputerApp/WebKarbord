a994d1c2892b2f6807677dbb5793ee8646dedefd
admin <admin@SERVER0>
2023-10-15 09:19:48 +0330

    ورژن قطعی 1032

--- WebKarbord/Controllers/Unit/UnitPublic.cs
+++ WebKarbord/Controllers/Unit/UnitPublic.cs
@@ -15,12 +15,12 @@ namespace WebKarbord.Controllers.Unit
 {
     public class UnitPublic
     {
-       public static string titleVer = "ورژن تست";
-       public static string titleVerNumber = "834"; 
+       //public static string titleVer = "ورژن تست";
+       //public static string titleVerNumber = "834"; 
 
         
-        //public static string titleVer = "ورژن"; 
-        //public static string titleVerNumber = "1031";
+        public static string titleVer = "ورژن"; 
+        public static string titleVerNumber = "1032";
 
         //public static string MAC;
         //public static string IP4;
